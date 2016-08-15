49469d7dcb105f4d183a28c495eeab9e365db8d3
amanuel2 <amanuelb43@gmail.com>
2016-08-15 15:55:54 -0400

    Enable Paging

--- boot.asm
+++ boot.asm
@@ -18,25 +18,27 @@ section .text
     extern page_directory
 	extern pages_init
     global loader
-    global enable_paging
-
-		enable_paging:
-			call pages_init
-			mov eax, page_directory
-
-
-			mov cr3, eax ; Load Page Directory to the cr3 Register
-
-			;TODO SET UP PSE (4MB Page Frame)
-
-			;Enable Paging
-			mov ebx, cr0
-			or ebx, 0x80000000 ; Set 31'th bit to 1
-			mov cr0, ebx ; Update the cr0 register
 
         loader:
                 mov esp,kernel_stack
                 call callConstructors
+
+                ;Enable Paging START
+
+	                call pages_init
+					mov eax, page_directory
+
+
+					mov cr3, eax ; Load Page Directory to the cr3 Register
+
+					;TODO SET UP PSE (4MB Page Frame)
+
+					;Enable Paging
+					mov ebx, cr0
+					or ebx, 0x80000000 ; Set 31'th bit to 1
+					mov cr0, ebx ; Update the cr0 register
+
+                ;Enable Paging END
                 push eax
                 push ebx
                 call kernelMain
