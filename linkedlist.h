a11c3e0b49af8a139a833566d9431ed635239191
amanuel2 <amanuelb43@gmail.com>
2016-09-09 23:06:04 -0400

    Prototype Linked List

--- linkedlist.h
+++ linkedlist.h
@@ -9,12 +9,40 @@
  */
 
 template <class T>
+static struct Node
+{
+	T info;
+	T *next;
+};
+
+enum
+{
+	I_GTL, //Integer Greatest To Least
+	I_LTG, //Integer Least to Greatest
+	S_GTL, //String Greatest to least
+	S_LTG  //String Least to greatest
+} SortingMethod;
+
+template <class T>
 class LinkedList
 {
 public:
 	LinkedList();
 	~LinkedList();
-	  T next;
+	void insert_begin();
+	void insert_end();
+	void insert_index(uint32_t index);
+	void insert_after_val(T val);
+	void insert_before_val(T val);
+	void remove_begin();
+	void remove_end();
+	void remove_index(uint32_t index);
+	void remove_after_val(T val);
+	void remove_before_val(T val);
+	void sort(SortingMethod sm);
+	void reverse();
+	uint32_t get_index(T val);
+	T get_val(uint32_t index);
 private:
 	uint32_t current_index=0;
 
